d865b0f23888e34baabedfaae0011599a09bc0b6
Hyacinto <jz.juhasz@gmail.com>
2024-11-09 20:55:37 +0100

    Google Sheets API authentication and basic data request

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,44 @@
 import express from "express"
 import cors from "cors"
+import { google } from "googleapis"
+import { GoogleAuth } from "google-auth-library"
+import dotenv from "dotenv"
+dotenv.config()
 
 const app = express()
 const port = 5000
 app.use(cors())
 app.use(express.json())
+
+const auth = new GoogleAuth({
+    credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_KEY),
+    scopes: [process.env.GOOGLE_SHEETS_SCOPE],
+})
+
+const sheets = google.sheets({ version: "v4", auth })
+
+app.get("/data", async (req, res) => {
+    try {
+        const spreadsheetId = "1515_6T0FgnIY-cz5vVVxnnUpSmas7jsn8ECJ7RqdjvA"
+        const range = "Tab1!A:C"
+
+        const response = await sheets.spreadsheets.values.get({
+            spreadsheetId,
+            range,
+        })
+
+        const rows = response.data.values
+
+        if (rows) {
+            res.json(rows);
+        } else {
+            res.status(404).json({ message: "Data not found" })
+        }
+    } catch (error) {
+        console.error("Error retrieving data:", error)
+        res.status(500).send("Error retrieving data")
+    }
+})
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
--- Frontend/currency_exchange/src/App.jsx
+++ Frontend/currency_exchange/src/App.jsx
@@ -1,10 +1,43 @@
 import './App.css'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [countryList, setCountryList] = useState([])
+  const [selectedCountry, setSelectedCountry] = useState("")
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://localhost:5000/data`)
+        if (!response.ok) throw new Error("Country list not found")
+        const result = await response.json()
+        setCountryList(result)
+      } catch (error) {
+        console.error("Error fetching data:", error)
+        setData(null)
+      }
+    }
+    fetchData()
+  }, [])
+
+  const handleSelectChange = (event) => {
+    setSelectedCountry(event.target.value)
+  }
 
   return (
     <>
-
+      <h1>Currency Change</h1>
+      <div className="form-group">
+        <label htmlFor="country-select">Select a country:</label>
+        <select id="country-select" value={selectedCountry} onChange={handleSelectChange}>
+          <option value="" disabled>Choose a country</option>
+          {countryList.map((country, index) => (
+            <option key={index} value={country[0]}>
+              {country[0]}
+            </option>
+          ))}
+        </select>
+      </div>
     </>
   )
 }
